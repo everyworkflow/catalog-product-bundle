15f14fbbba57010d8760cc187baec8ff5f5db2ae
Suson Waiba <susonwaiba@gmail.com>
2021-11-21 05:04:46 +0000

    Added ?ExtensionInterface

--- EveryWorkflowCatalogProductBundle.php
+++ EveryWorkflowCatalogProductBundle.php
@@ -9,11 +9,12 @@ declare(strict_types=1);
 namespace EveryWorkflow\CatalogProductBundle;
 
 use EveryWorkflow\CatalogProductBundle\DependencyInjection\CatalogProductExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EveryWorkflowCatalogProductBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new CatalogProductExtension();
     }
